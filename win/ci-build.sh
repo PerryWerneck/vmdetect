e7d2e1d9ad5b96fd44a55c1a2983da3d3c76aae1
Perry Werneck <perry.werneck@gmail.com>
2023-01-09 21:56:52 -0300

    Update ci-build.sh

--- win/ci-build.sh
+++ win/ci-build.sh
@@ -5,6 +5,7 @@
 #	* https://www.msys2.org/docs/ci/
 #
 #
+echo "Running ${readlink -f ${0}"
 
 LOGFILE=build.log
 rm -f ${LOGFILE}
@@ -20,6 +21,7 @@ cd $(dirname $(dirname $(readlink -f ${0})))
 #
 # Build LIBWMI++
 #
+echo "Building LIBWMI++"
 git clone https://github.com/Thomas-Sparber/wmi.git ./wmi > $LOGFILE 2>&1 || die "clone wmi++ failure"
 make -C wmi all  > $LOGFILE 2>&1 || die "Make failure"
 export WMI_CFLAGS="-I./wmi/include"
@@ -28,6 +30,7 @@ export WMI_LIBS="-Lwmi/build -lwmi -ldiaa_sami_comsupp -lwbemuuid -loleaut32 -lo
 #
 # Build VMDetect
 #
+echo "Building VMDETECT"
 ./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
 ./configure > $LOGFILE 2>&1 || die "Configure failure"
 make clean > $LOGFILE 2>&1 || die "Make clean failure"
