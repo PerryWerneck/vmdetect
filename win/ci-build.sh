fc8be1c5829ffa4acee8a93917a3f611e28e4ce5
Perry Werneck <perry.werneck@gmail.com>
2023-04-03 08:50:36 -0300

    Building mingw package.

--- win/ci-build.sh
+++ win/ci-build.sh
@@ -32,13 +32,13 @@ export WMI_LIBS="-Lwmi -lwmi -ldiaa_sami_comsupp -lwbemuuid -loleaut32 -lole32"
 # Build VMDetect
 #
 echo "Building VMDETECT"
-./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
-./configure > $LOGFILE 2>&1 || die "Configure failure"
-make clean > $LOGFILE 2>&1 || die "Make clean failure"
-make all  > $LOGFILE 2>&1 || die "Make failure"
-
+#./autogen.sh > $LOGFILE 2>&1 || die "Autogen failure"
+#./configure > $LOGFILE 2>&1 || die "Configure failure"
+#make clean > $LOGFILE 2>&1 || die "Make clean failure"
+#make all  > $LOGFILE 2>&1 || die "Make failure"
+makepkg BUILDDIR=/tmp/pkg -p PKGBUILD.mingw || die "makepkg failure"
 echo "Build complete"
 
-make DESTDIR=.bin/package install
-tar --create --xz --file=${MINGW_PACKAGE_PREFIX}-vmdetect.tar.xz --directory=.bin/package --verbose .
+#make DESTDIR=.bin/package install
+#tar --create --xz --file=${MINGW_PACKAGE_PREFIX}-vmdetect.tar.xz --directory=.bin/package --verbose .
 
